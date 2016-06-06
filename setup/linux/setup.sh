ae77c96aaa1d8c0e6a40a2c084bd7557d7708d2d
김대원 <james.kim@devsisters.com>
2016-06-06 22:51:52 +0900

    add linux setup script.

---
+++ setup/linux/setup.sh
@@ -0,0 +1,8 @@
+# input config
+# change key board stroke delay/rate
+xset r rate 200 100
+
+# install git breeze
+git clone git://github.com/ndbroadbent/scm_breeze.git ~/.scm_breeze
+~/.scm_breeze/install.sh
+source ~/.zshrc
