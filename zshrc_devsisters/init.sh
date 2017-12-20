ba708302a8a246da30692bd1dc4b197cd5996dfb
김대원 <james.kim@devsisters.com>
2017-12-20 10:49:20 +0900

    cleanup macbook zshrc.

--- zshrc_devsisters/init.sh
+++ zshrc_devsisters/init.sh
@@ -1,11 +1,6 @@
 CONFIG_PATH=$HOME/Develop/config
 export PATH=$PATH:$CONFIG_PATH/utils
-
-function my_source {
-    source $CONFIG_PATH/zshrc_devsisters/$1
-}
-
-my_source language.sh
-my_source proj_mars.sh
-my_source etc.sh
+source $CONFIG_PATH/zshrc_devsisters/language.sh
+source $CONFIG_PATH/zshrc_devsisters/proj_mars.sh
+source $CONFIG_PATH/zshrc_devsisters/etc.sh
 
--- zshrc_macbook/framework.sh
+++ zshrc_macbook/framework.sh
@@ -1,15 +1,6 @@
-export PATH="/usr/libexec/java_home/bin:$PATH" # java
-export PATH="/usr/local/share/dotnet:$PATH" # dotnet
-export PATH="/Library/Frameworks/Mono.framework/Versions/Current/bin:$PATH" # mono
+export PATH="$PATH:/usr/libexec/java_home/bin" # java
+export PATH="$PATH:/usr/local/share/dotnet" # dotnet
+export PATH="$PATH:/Library/Frameworks/Mono.framework/Versions/Current/bin" # mono
 
 # android
-ANDROID_HOME="/usr/local/Cellar/android-sdk/24.4.1_1"
-
-# jenkins
-#alias jenkins="java -jar ~/Develop/tools/jenkins-cli.jar -s http://localhost:8080/"
-
-#gcp
-# The next line updates PATH for the Google Cloud SDK.
-# if [ -f '/Users/jameskim/Develop/framework/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/jameskim/Develop/framework/google-cloud-sdk/path.zsh.inc'; fi
-# The next line enables shell command completion for gcloud.
-# if [ -f '/Users/jameskim/Develop/framework/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/jameskim/Develop/framework/google-cloud-sdk/completion.zsh.inc'; fi
+# ANDROID_HOME="/usr/local/Cellar/android-sdk/24.4.1_1"
