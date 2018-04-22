9a302486b2dad70c943566f9f472ee5cfbb1c6c9
김대원 <james.kim@devsisters.com>
2018-04-22 19:46:37 +0900

    update hammerspoon.

--- .hammerspoon/app/hotkey.lua
+++ .hammerspoon/app/hotkey.lua
@@ -6,13 +6,13 @@ local app_infos = {
     {'Google Chrome', 'C'},
     {'Slack', 'S'},
     {'Unity', 'U'},
-    {'Rider 2018.1.app', 'R'},
+    {'Rider 2018.1.app', 'I'},
 }
 
 for i, app_info in ipairs(app_infos) do
     local app_name = app_info[1]
     local app_key = app_info[2]
-    hs.hotkey.bind(mash_app, app_key,
+    hs.hotkey.bind(mash_app, app_key, '[App] Open ' .. app_name,
         function () hs.application.launchOrFocus(app_name) end)
 end
 
