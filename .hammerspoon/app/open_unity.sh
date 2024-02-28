7f7218c0e1a06e085117610739a44db03e01a612
James Kim <eodnjs2998@gmail.com>
2024-02-28 15:45:59 +0900

    wip

---
+++ .hammerspoon/app/open_unity.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+UNITY_APP=/Applications/Unity/Unity.app/Contents/MacOS/Unity
+UNITY_PROJ=$HOME/Develop/meow-tower
+
+# check if unity project open
+# process looks like this
+# /../Unity.app/Contents/MacOS/Unity -projectpath $UNITY_PROJ
+IS_OPEN=$(pgrep -f "Unity -projectpath $UNITY_PROJ")
+
+# if unity project is not open, open it
+if [ -z "$IS_OPEN" ]; then
+    open -a "$UNITY_APP" -n --args -projectpath "$UNITY_PROJ"
+# else focus on unity project
+else
+    osascript -e "tell application \"Unity\" to activate"
+fi
+
