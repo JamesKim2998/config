c33a24cb8670681f4e2c943960f61867fec57600
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-06-22 21:49:59 +0900

    shortcut for kitty

--- .hammerspoon/app/hotkey.lua
+++ .hammerspoon/app/hotkey.lua
@@ -1,26 +1,27 @@
 local mash_app = {"cmd", "alt", "ctrl"}
 
 local app_infos = {
-    {'Finder', 'F'},
-    {'Fork', 'K'},
-    {'Google Chrome', 'C'},
-    {'Notes', 'T'},
-    {'Notion', 'N'},
-    {'Reminders', 'M'},
-    {'Rider', 'R'},
-    {'Slack', 'S'},
-    {'WebStorm', 'W'},
+  {'Finder', 'F'},
+  {'Fork', 'K'},
+  {'Google Chrome', 'C'},
+  {'Notes', 'T'},
+  {'Notion', 'N'},
+  {'Reminders', 'M'},
+  {'Rider', 'R'},
+  {'Slack', 'S'},
+  {'WebStorm', 'W'},
+  {'kitty', 'Y'},
 }
 
 for i, app_info in ipairs(app_infos) do
-    local app_name = app_info[1]
-    local app_key = app_info[2]
-    hs.hotkey.bind(mash_app, app_key, 'Open ' .. app_name,
-        function () hs.application.launchOrFocus(app_name) end)
+  local app_name = app_info[1]
+  local app_key = app_info[2]
+  hs.hotkey.bind(mash_app, app_key, 'Open ' .. app_name,
+  function () hs.application.launchOrFocus(app_name) end)
 end
 
 -- Execute open_unity.sh for opening Unity.
 hs.hotkey.bind(mash_app, 'U', 'Open Unity', function ()
-    hs.execute('/Users/jameskim/Develop/meow-tower/!meow.app/Contents/MacOS/UnityLauncher', true)
+  hs.execute('/Users/jameskim/Develop/meow-tower/!meow.app/Contents/MacOS/UnityLauncher', true)
 end)
 
