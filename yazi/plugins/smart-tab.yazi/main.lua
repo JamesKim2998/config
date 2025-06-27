46d5d3f96b3184d709643564346730c7519474dc
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-06-27 09:22:17 +0900

    remove smart-tab (not working)

--- yazi/plugins/smart-tab.yazi/main.lua
+++
@@ -1,8 +0,0 @@
--- https://yazi-rs.github.io/docs/tips
---- @sync entry
-return {
-  entry = function()
-    local h = cx.active.current.hovered
-    ya.emit("tab_create", h and h.cha.is_dir and { h.url } or { current = true })
-  end,
-}
