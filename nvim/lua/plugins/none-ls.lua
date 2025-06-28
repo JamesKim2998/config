810fcaf07e78f774611a23f95f44e8c8a6424234
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-06-29 08:23:19 +0900

    setup none-ls

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,32 +1,37 @@
 return {
-  -- the plugin was renamed from “null-ls” → “none-ls” in 2024
-  "nvimtools/none-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },   -- lazy-load on first file
-  dependencies = { "nvim-lua/plenary.nvim" }, -- hard requirement:contentReference[oaicite:0]{index=0}
-  config = function()
-    local null_ls = require("null-ls")
-    local formatting  = null_ls.builtins.formatting
-    local diagnostics = null_ls.builtins.diagnostics
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	"nvimtools/none-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		-- allows you to easily manage external editor tooling such as LSP servers, DAP servers, linters, and formatters through a single interface.
+		-- https://github.com/mason-org/mason.nvim
+		"williamboman/mason.nvim",
+		-- The test suite includes unit and integration tests and depends on plenary.nvim.
+		-- https://github.com/nvim-lua/plenary.nvim
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    null_ls.setup({
-      sources = {
-        formatting.stylua,          -- Lua
-        formatting.prettier,        -- JS/TS/…  (needs prettier on PATH)
-        diagnostics.eslint_d,       -- JS/TS lint (fast daemon)
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
-    })
-  end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.completion.spell,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.selene, -- https://github.com/Kampfkarren/selene
+			},
+			-- https://github.com/nvimtools/none-ls.nvim/wiki/Formatting-on-save#code
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.formatting_sync()
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
