0c048aec1fddd2a4bb65c0fb8f54d24325c2afb5
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-06-29 08:58:52 +0900

    formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 require("config.lazy")
 
 -- Appearance
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme("catppuccin")
 
 -- Indentation vim.o.tabstop = 4 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
