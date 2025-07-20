e49e73778d170cd6c8c1ba8e8619bbc9f215f127
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-07-21 06:23:46 +0900

    wip

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
 #!/bin/zsh
+set -e
 
 CONFIG=$HOME/Develop/config
 
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install nvim fzf rg bat fd sd jq yq eza lsd zoxide lazygit delta gh yazi 7-zip imagemagick ffmpeg starship
+brew install \
+	nvim fzf rg bat fd sd jq yq clipboard \
+	eza lsd zoxide \
+	lazygit delta git-lfs gh \
+	lua rust dotnet \
+	7-zip imagemagick ffmpeg \
+	awscli \
+	yazi starship
+brew install --cask \
+	libreoffice
 
 # git
 ln -s $CONFIG/git/.gitconfig ~
