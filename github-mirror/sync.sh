a0aeba36787eb56b70985974529d581bc4c150ea
JK <eodnjs2998@gmail.com>
2026-01-07 18:30:55 +0900

    remove the github-mirror (extracted into individual repo)

--- github-mirror/sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Sync all GitHub mirror repos from origin (GitHub)
-for repo in ~/Develop/github-mirror/*.git; do
-    [ -d "$repo" ] && git -C "$repo" fetch --all -q 2>/dev/null
-done
